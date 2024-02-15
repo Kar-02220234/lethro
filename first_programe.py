03973dfcd86365d8680b6727ce04d3db50bbbe62
unknown <0222023.cst@rub.edu.bt>
2024-02-15 16:11:53 +0530

    changes to try.py file

---
+++ first_programe.py
@@ -0,0 +1,23 @@
+print("hello world")
+print("i fucking love programming")
+grade=99
+if grade>=90:
+    print("grade:A")
+elif grade>=80:
+    print("grade:B")
+elif grade>=60:
+    print("grade:C")
+elif grade>=40:
+    print("grade:D")
+else:
+    print("grade:F")
+age=21
+if age>=20:
+    print("your old enough to have sex")
+elif age<=18:
+    print("sorry boy your minor")
+elif age>=60:
+    print("your too old,you need vigra")
+else:
+    print("you are a gay")
+    
---
+++ try.py
@@ -0,0 +1,2 @@
+print("hello,it's me KAR")
+print("I am as cool as programe ICE")
