88759ff92650ea3599c130e63ab307706f3972ae
karma lethro <0222023.cst@rub.edu.bt>
2024-02-26 13:48:06 +0530

    logical.py change

---
+++ logical.py
@@ -0,0 +1,27 @@
+# AND operator(true if both true)
+
+# E1
+x = 5
+print(x > 3 and x < 10)
+# E2
+y = 12
+print(y > 10 and y % 5 == 0)
+
+# OR operator(true if one is true)
+# E1
+x = 5
+print(x < 3 or x > 10)
+# E2
+y = 12
+print(y < 10 or y % 2 == 0)
+
+# NOt operator(reverse)
+# E1
+x = 5
+print (not(x > 3 and x < 10))
+# E2
+y = 12
+print (not(y > 10 and y % 5 == 0))
+
+
+
